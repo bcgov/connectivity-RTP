022f28c56e53e62fb7eeda821091d408f505c017
Bryan Krauss <bryan@button.is>
2021-12-20 16:23:10 -0800

    feat: creates Menu component for Footer

---
+++ app/components/Menu.js
@@ -0,0 +1,23 @@
+export default function Menu() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href=".">
+            About
+          </a>
+        </li>
+        <li>
+          <a href=".">
+            Careers
+          </a>
+        </li>
+        <li>
+          <a href=".">
+            Privacy Policy
+          </a>
+        </li>
+      </ul >
+    </>
+  )
+}
