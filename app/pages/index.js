a3b8e2dbb9700fbfb188de419d55fdd438351b2b
Bryan Krauss <bryan@button.is>
2021-12-21 08:55:29 -0800

    feat: adds Jsonschema form and schema files

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Footer, Navigation } from '@button-inc/bcgov-theme';
 import Menu from '../components/Menu';
+import JsonSchemanForm from 'react-jsonschema-form';
+import schema from '../schemas/schema';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -21,6 +23,9 @@ export default function Home() {
         title="Welcome to Connectivity Intake"
       />
       <Title>Connectivity Intake Form</Title>
+      <JsonSchemanForm
+        schema={schema}
+      />
       <SFooter>
         <Menu />
       </SFooter>
---
+++ app/schemas/schema.js
@@ -0,0 +1,42 @@
+const schema = {
+  "required": [
+    "Full Name"
+  ],
+  "properties": {
+    "Full Name": {
+      "title": "Full Name",
+      "type": "string"
+    },
+    "Group1": {
+      "properties": {
+        "First": {
+          "title": "Question #1",
+          "type": "string"
+        },
+        "Second": {
+          "title": "Question #2",
+          "type": "string"
+        }
+      },
+      "type": "object",
+      "description": "Grouped Fields 1"
+    },
+    "Group2": {
+      "properties": {
+        "First": {
+          "title": "Question #3",
+          "type": "string"
+        },
+        "Second": {
+          "title": "Question #4",
+          "type": "string"
+        }
+      },
+      "type": "object",
+      "description": "Grouped Fields 2"
+    }
+  },
+  "title": "Connectivity Intake"
+};
+
+export default schema;
