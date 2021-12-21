5dc9560ad232e2f993cb7b51986939f7bf0f5a60
Bryan Krauss <bryan@button.is>
2021-12-20 16:23:35 -0800

    feat: changes to index.js for nav, footer(+styling)

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,29 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { Footer, Navigation } from '@button-inc/bcgov-theme';
+import Menu from '../components/Menu';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const SFooter = styled(Footer)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <Navigation
+        header="main"
+        title="Welcome to Connectivity Intake"
+      />
+      <Title>Connectivity Intake Form</Title>
+      <SFooter>
+        <Menu />
+      </SFooter>
+    </>
+  )
 }
