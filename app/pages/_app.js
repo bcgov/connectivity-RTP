6fffca3dd02fc901b34eb8b38c8d24f7e52c10fe
Bryan Krauss <bryan@button.is>
2021-12-28 13:13:11 -0800

    refactor: moves nav and footer to _app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,4 +1,7 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { Footer, Navigation } from '@button-inc/bcgov-theme';
+import styled from 'styled-components';
+import Menu from '../components/Menu';
 import BCGovTypography from '../components/BCGovTypography';
 
 const GlobalStyle = createGlobalStyle`
@@ -15,13 +18,28 @@ const theme = {
   },
 };
 
+const SFooter = styled(Footer)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <BCGovTypography />
+        <Navigation
+          header="main"
+          title="Connectivity Intake"
+        >
+          <Menu />
+        </Navigation>
         <Component {...pageProps} />
+        <SFooter>
+          <Menu />
+        </SFooter>
       </ThemeProvider>
     </>
   )
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,69 +1,17 @@
 import styled from 'styled-components';
-import { Footer, Navigation, Button } from '@button-inc/bcgov-theme';
-import Menu from '../components/Menu';
-import JsonSchemaForm from '@rjsf/semantic-ui';
-import schema from '../schemas/schema';
-import uiSchema from '../schemas/uiSchema';
-import Form from '../components/Form';
+import Link from 'next/link';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-const SFooter = styled(Footer)`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-`;
-
-const SJsonSchemaForm = styled(JsonSchemaForm)`
-  max-width: 90% !important;
-  margin: auto !important;
-  margin-top: 25px !important;
-`;
-
-const handleSubmit = ({ formData }, e) => {
-  console.log(formData);
-};
-
-export default function Home({ formData }) {
+export default function Home() {
   return (
     <>
-      <Navigation
-        header="main"
-        title="Connectivity Intake"
-      >
-        <Menu />
-      </Navigation>
-      <Form />
-      {/* <SJsonSchemaForm
-        name="connectivity-intake"
-        formData={formData}
-        schema={schema}
-        uiSchema={uiSchema}
-        onSubmit={handleSubmit}
-      >
-        <>
-          <Button
-            size="medium"
-            variant="primary"
-            type="submit"
-          >
-            Submit
-          </Button>
-          <Button
-            size="medium"
-            variant="secondary"
-            type="reset"
-          >
-            Cancel
-          </Button>
-        </>
-      </SJsonSchemaForm> */}
-      <SFooter>
-        <Menu />
-      </SFooter>
+      <Link href="/1">
+        &#9997; Begin Form!
+      </Link>
     </>
   )
 };
