523e047558de4b7c22d434afb09ef9f08d7191a2
Alec Wenzowski <alec@button.is>
2021-12-30 15:30:08 -0800

    feat: add technology & pre-screen questions

--- app/schemas/uiSchema.js
+++ app/schemas/uiSchema.js
@@ -1,5 +1,26 @@
 const uiSchema = {
-
+  "preScreen": {
+    "nonISPCollaborationPlan": {
+      "ui:widget": "textarea"
+    },
+    "ISPNetworkExperience": {
+      "ui:widget": "textarea"
+    },
+    "ISPTechnologyExperience": {
+      "ui:widget": "textarea"
+    }
+  },
+  "technologies": {
+    "transport": {
+      "ui:widget": "checkboxes"
+    },
+    "lastMile": {
+      "ui:widget": "checkboxes"
+    },
+    "highway": {
+      "ui:widget": "checkboxes"
+    }
+  }
 };
 
 export default uiSchema;
