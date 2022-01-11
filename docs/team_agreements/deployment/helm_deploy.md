4f56d683efe5566fcf17d53d9f4f48bc8426036e
Bryan Krauss <bryan@button.is>
2022-01-11 13:53:14 -0800

    docs: documentation for deploying
    refactor: fixes minor errors
    
    refactor: changes to network policies
    
    refactor: changes to network policy
    
    refactor: more network policy changes
    
    refactor: changes to selector for routing
    
    feat: adds GA for deploying to openshift
    
    fix: minor fix on deploy GA
    
    refactor: moves deploy GA to publish GA file
    
    refactor: making it work
    
    refactor: secrets to github
    
    refactor: removes checkout for GA
    
    refactor: moves around checkout cmd
    
    refactor: adds dash
    
    refactor: period to path
    
    refactor: add debug and pipefail
    
    refactor: moves --set in helm_deploy.sh
    
    refactor: changes how tag is ref'd
    
    refactor: changes to push
    
    refactor: changes to run on push to main

---
+++ docs/team_agreements/deployment/helm_deploy.md
@@ -0,0 +1,8 @@
+# Deployment
+
+## Deploy using Helm charts
+To deploy the connectivity intake using Helm charts run the following commands:
+```bash
+cd lib
+bash helm_deploy.sh <namespace> ../helm
+```
--- lib/helm_deploy.sh
+++ lib/helm_deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 NAMESPACE=$1
 CHART_PATH=$2
-echo "Creating Helm installation in $NAMESPACE namespace"
+TAG=$3
+echo "Creating Helm installation in $NAMESPACE namespace with tag $TAG"
 
-helm upgrade --install --atomic -n $NAMESPACE connectivity-intake $CHART_PATH
+helm upgrade --install --atomic --set image.app.tag=$TAG -n $NAMESPACE connectivity-intake $CHART_PATH --debug
