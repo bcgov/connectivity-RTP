3641f03263fe21b69d801227639efe8a2bfb9c43
Bryan Krauss <bryan@button.is>
2022-01-12 15:54:36 -0800

    fix: fixes helm script

--- lib/helm_deploy.sh
+++ lib/helm_deploy.sh
@@ -6,4 +6,6 @@ NAMESPACE=$1
 TAG=$2
 echo "Creating Helm installation in $NAMESPACE namespace with tag $TAG"
 
-helm upgrade --install --atomic -f ./helm/values.yaml --set image.app.tag=$TAG -n $NAMESPACE connectivity-intake ./helm --debug
+helm upgrade --install --atomic \
+  -f ./helm/values.yaml --set image.app.tag=$TAG \
+  -n $NAMESPACE connectivity-intake ./helm --debug
