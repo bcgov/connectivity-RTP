4d564e73430081597bd49e4958798448ab7624a2
Bryan Krauss <bryan@button.is>
2022-01-12 14:18:26 -0800

    refactor: removes helm chart path as env entry

--- lib/helm_deploy.sh
+++ lib/helm_deploy.sh
@@ -3,8 +3,7 @@
 set -euxo pipefail
 
 NAMESPACE=$1
-CHART_PATH=$2
-TAG=$3
+TAG=$2
 echo "Creating Helm installation in $NAMESPACE namespace with tag $TAG"
 
-helm upgrade --install --atomic -f ./helm/values.yaml --set image.app.tag=$TAG -n $NAMESPACE connectivity-intake $CHART_PATH --debug
+helm upgrade --install --atomic -f ./helm/values.yaml --set image.app.tag=$TAG -n $NAMESPACE connectivity-intake ./helm --debug
