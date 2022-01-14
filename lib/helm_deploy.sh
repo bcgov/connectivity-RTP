5376d9a2a177683d7ca261f24060a6f777421d46
Bryan Krauss <bryan@button.is>
2022-01-14 13:06:10 -0800

    feat: adds dependency to shell deploy script

--- lib/helm_deploy.sh
+++ lib/helm_deploy.sh
@@ -6,6 +6,7 @@ NAMESPACE=$1
 TAG=$2
 echo "Creating Helm installation in $NAMESPACE namespace with tag $TAG"
 
+helm dep up &&
 helm upgrade --install --atomic \
   -f ./helm/values.yaml --set image.app.tag=$TAG \
-  -n $NAMESPACE connectivity-intake ./helm --debug
+  -n $NAMESPACE connectivity-intake ./helm --debug --dry-run
