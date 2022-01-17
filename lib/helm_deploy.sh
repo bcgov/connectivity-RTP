23610fc6bba83e853521696a79dd7edef883266b
Bryan Krauss <bryan@button.is>
2022-01-17 09:05:40 -0800

    refactor: optimizes helm_deploy script

--- lib/helm_deploy.sh
+++ lib/helm_deploy.sh
@@ -6,9 +6,8 @@ NAMESPACE=$1
 TAG=$2
 echo "Creating Helm installation in $NAMESPACE namespace"
 
-pushd ./helm
+cd helm
 helm dep up
-popd
 helm upgrade --install --atomic \
-  -f ./helm/values.yaml --set image.app.tag=$TAG \
-  -n $NAMESPACE connectivity-intake ./helm --debug
+  -f ./values.yaml --set image.app.tag=$TAG \
+  -n $NAMESPACE connectivity-intake . --debug
