4c969ebc179a2e6c9e8d5d6fd21617ab0eec4fb1
Bryan Krauss <bryan@button.is>
2022-02-18 08:20:34 -0800

    feat: onPost function in form-schema

--- app/form-schema.js
+++ app/form-schema.js
@@ -6,10 +6,15 @@ import postData from './utils/post-data'
 const options = {
   getRoute: '/form',
   postRoute: '/api',
-  useSession: true,
+  useSession: false,
   onFormEnd: (errors, formData, req) => {
     if (errors) throw new Error("There was an error saving your information: ", errors);
     postData(formData, req);
+  },
+  onPost: (_postData, schemaIndex, cleanSchemaData) => {
+    const newData = cleanSchemaData(formData);
+    formData[schemaIndex] = { ...formData[schemaIndex], ...newData };
+    return formData[schemaIndex];
   },
   validateEachPage: true,
   validatedUrl: '/end',
