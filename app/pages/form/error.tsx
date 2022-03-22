876e69c774960579bd04cd27ebd662843e2a8741
Bryan Krauss <bryan@button.is>
2022-03-22 13:52:54 -0700

    style: form error page UI changes

--- app/pages/form/error.tsx
+++ app/pages/form/error.tsx
@@ -1,10 +1,30 @@
-import MainStyledDiv from "../../components/MainStyledDiv"
+import Card from "@button-inc/bcgov-theme/Card"
+import Link from "@button-inc/bcgov-theme/Link"
+import LandingStyledDiv from "../../components/LandingStyledDiv"
+import getConfig from "next/config"
+
+const runtimeConfig = getConfig()?.publicRuntimeConfig ?? {};
+const landingPage =
+  runtimeConfig.NODE_ENV === "production"
+    ? `https://${runtimeConfig.HOST}`
+    : `http://${runtimeConfig.HOST}:${runtimeConfig.PORT}`;
 
 export default function error() {
   return (
-    <MainStyledDiv>
-      <h1>Oops... something has gone wrong.</h1>
-      <p>Try hitting the back button or returning <a href="/home">home</a></p>
-    </MainStyledDiv>
+      <LandingStyledDiv>
+        <Card title="Form Error">
+          <p>
+            Don't worry, your responses were auto-saved the last time you pressed Continue.
+          </p>
+          <p>
+            Please <Link href={landingPage}>
+              return back to the main page
+            </Link> and log back in.
+          </p>
+          <p>
+            If that doesn't work, trying clearing your cookies then logging in again.
+          </p>
+          </Card>
+        </LandingStyledDiv>
   )
 }
