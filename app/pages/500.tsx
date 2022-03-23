67a33be529913ca74fb4e38a7f6b0b5a4ec38ad8
Bryan Krauss <bryan@button.is>
2022-03-23 13:52:19 -0700

    chore: re-deploy to dev server

--- app/pages/500.tsx
+++ app/pages/500.tsx
@@ -19,7 +19,7 @@ export default function Custom500() {
             If that doesn't work, please clear any cookies and restart your browser.
           </p>
           </Card>
-        </LandingStyledDiv> 
+        </LandingStyledDiv>
     </>
   )
 }
