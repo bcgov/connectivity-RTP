5a2b60f1bac91f883316559757294544b4cf9238
Bryan Krauss <bryan@button.is>
2022-03-23 08:12:44 -0700

    style: ui changes to error pages

--- app/pages/500.tsx
+++ app/pages/500.tsx
@@ -13,17 +13,17 @@ export default function Custom500() {
   return (
     <>
       <LandingStyledDiv>
-        <Card title="500 Error - Server Error">
+        <Card title="500 Error - Internal Server Error">
           <p>
-            Don't worry, your responses were auto-saved the last time you pressed Continue.
+            Your responses were auto-saved the last time you pressed Continue.
           </p>
           <p>
             Please <Link href={landingPage}>
-              return back to the main page
+              return home
             </Link> and log back in.
           </p>
           <p>
-            If that doesn't work, trying clearing your cookies then logging in again.
+            If that doesn't work, please clear any cookies and restart your browser.
           </p>
           </Card>
         </LandingStyledDiv>
