ddabf2a73e1f10b571d36ea4ce48410774ab63f6
Bryan Krauss <bryan@button.is>
2022-03-24 08:37:46 -0700

    refactor: move app helm chart to app subfolder

--- lib/helm_deploy.sh
+++ lib/helm_deploy.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 echo "Creating Helm installation $*"
 
-cd helm
+cd helm/app
 helm dep up
 helm upgrade --install --atomic -f ./values.yaml "$@" connectivity-intake . \
   --debug --timeout=8m0s
